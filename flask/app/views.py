bceb4149cfd7c282769864fdd70ad80cf17c451f
blairg23 <blairg23@gmail.com>
2016-09-10 03:03:28 -0600

    Stopped work on angularjs project and started rewriting site in flask using bootstrap

---
+++ flask/app/views.py
@@ -0,0 +1,70 @@
+from flask import render_template
+from app import app
+
+import json
+
+@app.route('/')
+@app.route('/index')
+def index():
+	page = {
+			"title": "Home Page",
+			"jumbotron": "Welcome to Fabricon!",
+			"imgs": [
+						"/static/img/1.jpg",
+						"/static/img/2.jpg",
+						"/static/img/3.jpg"
+					]
+	}
+	return render_template(
+							'index.html',
+							page=page							
+						)
+
+@app.route('/about')
+def about():
+	page = {
+			"title": "About Fabricon",
+			"jumbotron": "About Fabricon."
+	}
+	return render_template(
+							'about.html',							
+							page=page
+						)
+
+@app.route('/contact')
+def contact():
+	page = {
+			"title": "Contact Us",
+			"jumbotron": "Contact Us."
+	}
+	return render_template(
+							'contact.html',
+							page=page
+						)
+
+@app.route('/products')
+def products():
+	page = {
+			"title": "Products",
+			"jumbotron": "Products."
+	}
+	return render_template(
+							'products.html',
+							page=page							
+						)
+
+@app.route('/user')
+@app.route('/user/<string:username>')
+def user(username='Mr. McDefault'):
+	username = username
+	user = {'nickname': username}  # fake user
+	with open('app/posts.json', 'r') as infile:
+		data = json.load(infile)
+	posts = data['posts']
+	print posts
+	return render_template(
+							'users.html',
+							title='Home',
+							user=user,
+							posts=posts
+						)
