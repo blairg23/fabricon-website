9bcc02761d68eb83a6c5944f6bdf157bea86a66b
blair gemmer <blairg23@gmail.com>
2018-07-20 19:46:03 -0600

    Initial commit, moved theme files over

--- README.md
+++ README.md
@@ -3,7 +3,3 @@ fabricon
 
 Website for Fabricon, LLC
 
-
-# To run:
-
-Type `Python run.py` from the root directory.
--- run.py
+++
@@ -1,5 +0,0 @@
-#!flask/bin/python
-from app import app as application
-
-if __name__ == "__main__":
-	application.run(host="0.0.0.0", debug=False)
