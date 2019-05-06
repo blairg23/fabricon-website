45100d6ef0ad73cf1b1e9e10a40faedd7aab5eae
blairg23 <blairg23@gmail.com>
2019-05-05 19:34:25 -0600

    Updated gunicorn and nginx config files.

--- backend/server_config/gunicorn/gunicorn_start.sh
+++ backend/server_config/gunicorn/gunicorn_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-NAME="ourcase"                              #Name of the application (*)
-DJANGODIR=/var/www/root/fabricon-website             # Django project directory (*)
-SOCKFILE=/var/www/html/run/gunicorn.sock        # we will communicate using this unix socket (*)
-USER=nginx                                        # the user to run as (*)
-GROUP=webdata                                     # the group to run as (*)
-NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
-DJANGO_SETTINGS_MODULE=server.settings             # which settings file should Django use (*)
-DJANGO_WSGI_MODULE=server.wsgi                     # WSGI module name (*)
+NAME="fabricon"                                     #Name of the application (*)
+DJANGODIR=/var/www/html/fabricon-website/backend    # Django project directory (*)
+SOCKFILE=/var/www/html/run/gunicorn.sock            # we will communicate using this unix socket (*)
+USER=root                                           # the user to run as (*)
+GROUP=webdata                                       # the group to run as (*)
+NUM_WORKERS=1                                       # how many worker processes should Gunicorn spawn (*)
+DJANGO_SETTINGS_MODULE=server.settings              # which settings file should Django use (*)
+DJANGO_WSGI_MODULE=server.wsgi                      # WSGI module name (*)
 
 echo "Starting $NAME as `whoami`"
 
@@ -23,7 +23,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ~/.virtualenvs/fabricon/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user $USER \
