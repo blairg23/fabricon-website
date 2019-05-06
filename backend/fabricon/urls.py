c2f64ae1d5210621f68863df27ca2613ffb34d82
blairg23 <blairg23@gmail.com>
2019-05-05 20:08:53 -0600

    Removed media files reference from urls.py

--- backend/fabricon/urls.py
+++ backend/fabricon/urls.py
@@ -31,6 +31,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
